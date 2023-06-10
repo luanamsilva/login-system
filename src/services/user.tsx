8ac5606009662e2ab3155f04cf5e059071784418
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-10 14:31:47 -0300

    add-try-catch

--- src/services/user.tsx
+++ src/services/user.tsx
@@ -11,4 +11,6 @@ export function register(body) {
 export function login(body) {
   const user = users.find(({email}) => email === body.email)
   if (!user) throw new Error("Usuário não encontrado")
+  if (user.password !== body.password) throw new Error('Senha incorreta')
+  return user
 }
