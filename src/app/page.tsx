7586570939363071cce0fb032db7735c1093b4dd
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-11 18:03:49 -0300

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import LoginCard from '../components/LoginCard'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import Link from 'next/link'
+
+
+
+export default function App() {
+
+
+  
   return (
     <main>
-<h1>Página Home</h1>
+
+<LoginCard title='Para ter acesso ao conteúdo informe seus dados de acesso' label='Login:'>
+<Input className="pl-2 bg-gray-100 rounded-md focus:outline-blue-400" type="email" placeholder="Digite seu e-mail"/>
+<Input  className="pl-2 rounded-md focus:outline-blue-400" type="passord" placeholder="Digite sua senha"/>
+<Button >Enviar</Button>
+<Link className="text-gray-300" href='/register'>Ainda não possui cadastro?</Link>
+  </LoginCard>
     </main>
   )
 }
