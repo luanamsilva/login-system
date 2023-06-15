f2299300d434da7c465d9bfe3c59df5606b13275
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-15 09:45:50 -0300

    add-next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
-import LoginCard from '../components/LoginCard'
-import Input from '../components/Input'
-import Button from '../components/Button'
-import Link from 'next/link'
+'use client';
 
-
+import LoginCard from '../components/LoginCard';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 
 export default function App() {
+  // const { data, status } = useSession();
 
-
-  
   return (
     <main>
-
-<LoginCard title='Para ter acesso ao conteúdo informe seus dados de acesso' label='Login:'>
-<Input className="pl-2 bg-gray-100 rounded-md focus:outline-blue-400" type="email" placeholder="Digite seu e-mail"/>
-<Input  className="pl-2 rounded-md focus:outline-blue-400" type="passord" placeholder="Digite sua senha"/>
-<Button >Enviar</Button>
-<Link className="text-gray-300" href='/register'>Ainda não possui cadastro?</Link>
-  </LoginCard>
+      <LoginCard
+        title="Para ter acesso ao conteúdo informe seus dados de acesso"
+        label="Login:"
+      >
+        <Input
+          className="pl-2 bg-gray-100 rounded-md focus:outline-blue-400"
+          type="email"
+          placeholder="Digite seu e-mail"
+        />
+        <Input
+          className="pl-2 rounded-md focus:outline-blue-400"
+          type="passord"
+          placeholder="Digite sua senha"
+        />
+        <Button>Enviar</Button>
+        <Link className="text-gray-300" href="/register">
+          Ainda não possui cadastro?
+        </Link>
+      </LoginCard>
     </main>
-  )
+  );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,20 +1,35 @@
-import LoginCard from '../../components/LoginCard'
-import Input from '../../components/Input'
-import Button from '../../components/Button'
-import Link from 'next/link'
+import LoginCard from '../../components/LoginCard';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Link from 'next/link';
 
 export default function RegisterPage() {
   return (
     <main>
-
-<LoginCard  title='Faça seu cadastro para ter acesso' label='Nome do usuário:'>
-
-<Input  className="pl-2 rounded-md focus:outline-blue-400" type="text" placeholder="Digite seu nome"/>
-<Input className="pl-2 rounded-md focus:outline-blue-400"  type="email" placeholder="Digite seu e-mail"/>
-<Input  className="pl-2 rounded-md focus:outline-blue-400" type="passord" placeholder="Digites sua senha"/>
-<Button>Cadastrar</Button>
-<Link className="text-gray-300" href='/login'>Clique aqui se já possui uma conta</Link>
-</LoginCard>
+      <LoginCard
+        title="Faça seu cadastro para ter acesso"
+        label="Nome do usuário:"
+      >
+        <Input
+          className="pl-2 rounded-md focus:outline-blue-400"
+          type="text"
+          placeholder="Digite seu nome"
+        />
+        <Input
+          className="pl-2 rounded-md focus:outline-blue-400"
+          type="email"
+          placeholder="Digite seu e-mail"
+        />
+        <Input
+          className="pl-2 rounded-md focus:outline-blue-400"
+          type="passord"
+          placeholder="Digites sua senha"
+        />
+        <Button>Cadastrar</Button>
+        <Link className="text-gray-300" href="/login">
+          Clique aqui se já possui uma conta
+        </Link>
+      </LoginCard>
     </main>
-  )
+  );
 }
